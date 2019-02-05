12468711ce3c74d658482de5fdcf4fdd24d56b89
Michael Chan <mijoch@gmail.com>
2019-02-05 10:11:05 -0800

    add @planningcenter/browserlist-config to design

---
+++ planningcenter/browserslist-config/README.md
@@ -0,0 +1,17 @@
+# `browserslist-config`
+
+Our browser support codified for Node environments
+
+## Installation
+
+```bash
+npm i @planningcenter/browserlist-config
+```
+
+## Usage
+
+```
+"browserslist": [
+  "extends @planningcenter/browserslist-config"
+]
+```
---
+++ planningcenter/browserslist-config/browserslist-config.js
@@ -0,0 +1,15 @@
+"use strict";
+
+module.exports = [
+  "last 2 Firefox major version",
+  "last 2 Chrome major version",
+  "last 2 edge major versions",
+  "Explorer 11",
+  "ExplorerMobile 11",
+  "last 2 Safari major versions",
+  "last 2 iOS major versions",
+  "last 2 ChromeAndroid major versions",
+  "last 2 FirefoxAndroid major versions",
+  "UCAndroid 11.8",
+  "Android 4.4.4"
+];
