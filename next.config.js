8392c04f1bfe826f585bb2d4f1efc72ec530e7d6
Michael Chan <mijoch@gmail.com>
2019-04-22 12:51:58 -0700

    add config for eventual styling of docs

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
+const rehypePrism = require("@mapbox/rehype-prism");
+
 const withMDX = require("@zeit/next-mdx")({
   extension: /.mdx?$/
 });
+
 const withCSS = require("@zeit/next-css");
 
 module.exports = withCSS(
   withMDX({
-    pageExtensions: ["js", "jsx", "md", "mdx"]
+    extension: /.mdx?$/,
+    pageExtensions: ["js", "md", "mdx"],
+    options: { hastPlugins: [rehypePrism] }
   })
 );
