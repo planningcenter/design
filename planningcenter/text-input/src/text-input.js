67e413b28b10f14193fec3c2f7d7284143d110f2
Michael Chan <mijoch@gmail.com>
2019-10-23 00:43:20 -0700

    text-input: doh, remove export default

--- planningcenter/text-input/src/text-input.js
+++ planningcenter/text-input/src/text-input.js
@@ -5,7 +5,7 @@ export const SIZES = Array.from(
   new Set([2.5, ...SYSTEM_SIZES.filter(s => s >= 3 && s <= 5)])
 ).sort();
 
-export default function TextInput({ as: As = "input", className, ...props }) {
+export function TextInput({ as: As = "input", className, ...props }) {
   return (
     <As className={[className, "TextInput"].join(" ").trim()} {...props} />
   );
