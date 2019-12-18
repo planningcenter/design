0d2bd7259379d51328299654334236fe12a171cf
Michael Chan <mijoch@gmail.com>
2019-12-18 10:16:04 -0800

    remove next.js config

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testPathIgnorePatterns: [
-    "<rootDir>/build/",
-    "<rootDir>/pages/",
-    "<rootDir>/.next/",
-    "<rootDir>/pages_support/",
-    "<rootDir>/node_modules/"
-  ]
-};
