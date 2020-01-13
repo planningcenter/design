b2feb43df8a699159f744fbb7b57198bf0005b96
Michael Chan <mijoch@gmail.com>
2020-01-13 14:17:23 -0800

    url: add custom tld support to pco. domains

---
+++ planningcenter/url/CHANGELOG.md
@@ -0,0 +1,3 @@
+# Next
+
+- Add `.codes` support for `pco-box`
--- planningcenter/url/index.js
+++ planningcenter/url/index.js
@@ -7,7 +7,18 @@ export default env => appName => {
     case "staging":
       return `https://${appName}-staging.planningcenteronline.com`;
     case "development":
-      return `http://${appName}.pco.test`;
+      let tld = "test";
+
+      try {
+        tld = location.hostname.match(/\pco\.(\w+)$/)[1];
+      } catch (error) {
+        // don't show warning in Topbar's local dev env
+        if (location.hostname !== "localhost") {
+          console.error(`${location.hostname} is not a supported dev TLD`);
+        }
+      }
+
+      return `http://${appName}.pco.${tld}`;
     case "test":
       return `http://${appName}.pco.test`;
     default:
