2434c2293f8d367c4f1403c263d8a9248cdd27a2
Michael Chan <mijoch@gmail.com>
2020-02-06 15:15:52 -0800

    components: text-input: remove extensions and build base around 16px

--- planningcenter/components/src/composed-text-input.js
+++ planningcenter/components/src/composed-text-input.js
@@ -1,13 +1,12 @@
 import React from "react";
 
 import { TextInput as TextInputElement } from "./text-input/text-input";
-import { getPointGridClassNames } from "./text-input/extension/point-grid";
 import { getInvalidClassNames } from "./text-input/extension/invalid";
 
 import { SIZES as SYSTEM_SIZES } from "@planningcenter/system";
 
 export const SIZES = Array.from(
-  new Set([2.5, ...SYSTEM_SIZES.filter(s => s >= 3 && s <= 5)])
+  new Set([2.5, ...SYSTEM_SIZES.filter((s) => s >= 3 && s <= 5)])
 ).sort();
 
 export function TextInput({
@@ -19,11 +18,7 @@ export function TextInput({
 }) {
   return (
     <TextInputElement
-      className={[
-        className,
-        getPointGridClassNames({ height }, strict && SIZES),
-        getInvalidClassNames({ invalid })
-      ]
+      className={[className, getInvalidClassNames({ invalid })]
         .join(" ")
         .trim()}
       {...props}
