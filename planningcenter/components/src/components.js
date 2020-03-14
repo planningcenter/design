9ba323a330119b52e2405280374c3bf263846200
Michael Chan <mijoch@gmail.com>
2020-03-14 12:15:09 -0700

    remove /compnonets src

--- planningcenter/components/src/components.js
+++
@@ -1,1 +0,0 @@
-export { Checkbox, SIZES as CHECKBOX_SIZES } from "./composed-checkbox";
--- planningcenter/components/src/composed-checkbox.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { Checkbox as CheckboxElement } from "./checkbox/checkbox";
-
-export const SIZES = [];
-
-export function Checkbox({ ...props }) {
-  return <CheckboxElement {...props} />;
-}
