7d282eaf13c3419b846a73e5a0f98702a10b24af
Michael Chan <mijoch@gmail.com>
2020-06-23 22:52:09 -0700

    add storybook config for churchcenter/publishing-status

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,6 +3,9 @@ import "./global.css";
 
 // automatically import all files ending in *.stories.js
 configure(
-  require.context("../planningcenter", true, /\.stories\.(js|mdx)$/),
+  [
+    require.context("../planningcenter", true, /\.stories\.(js|mdx)$/),
+    require.context("../churchcenter", true, /\.stories\.(js|mdx)$/),
+  ],
   module
 );
