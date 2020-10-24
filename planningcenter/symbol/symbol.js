7029325f503a35a809aeea53c090294863f6fba8
Michael Chan <mijoch@gmail.com>
2020-10-24 00:44:37 -0700

    add improved title/desc handling trialed in groups and registrations

--- planningcenter/symbol/symbol.js
+++ planningcenter/symbol/symbol.js
@@ -1,18 +1,43 @@
 import React from "react";
+import uniqueId from "lodash.uniqueid";
 
-function Symbol({ className, symbol: _symbol, ...platformProps }) {
+export const ForceTitleContext = React.createContext(false);
+
+export function Symbol({
+  className,
+  symbol: _symbol,
+  title: _title = "",
+  description = "",
+  role = "img",
+  ...platformProps
+}) {
+  let useSymbolAsTitle = React.useContext(ForceTitleContext);
+
   let [path, symbol] = _symbol.replace(".svg", "").split("#");
+  let uid = uniqueId(`${symbol}-`);
+  let title = _title ? _title : useSymbolAsTitle ? symbol : "";
+  let optionalAttrs =
+    title || description
+      ? {
+          "aria-labelledby": [
+            title && `title-${uid}`,
+            description && `desc-${uid}`,
+          ]
+            .filter(Boolean)
+            .join(" "),
+        }
+      : null;
 
   return (
     <svg
-      role="img"
-      title={`${symbol} icon`}
+      role={role}
       className={["symbol", className].join(" ").trim()}
+      {...optionalAttrs}
       {...platformProps}
     >
+      {title && <title id={`title-${uid}`}>{title}</title>}
+      {description && <desc id={`desc-${uid}`}>{description}</desc>}
       <use xlinkHref={`${path}.svg#${symbol}`} />
     </svg>
   );
 }
-
-export default Symbol;
